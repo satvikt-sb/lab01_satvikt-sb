97e0e6998f17575cfb4afa38c9c861c8074eca6b
satvikt-sb <satviktalchuru@ucsb.edu>
2024-10-16 22:10:51 -0700

    Initial version of lab01

--- car.cpp
+++ car.cpp
@@ -1,7 +1,116 @@
-// Name(s) and Perm
+// Satvik Talchuru A0P9K47
 // This is the only file to submit.
 
 #include "car.hpp"
 #include <cstddef>
 #include <cstring>
 // #include .......
+
+Car::Car() {
+    manufacturer = nullptr;
+    model = nullptr;
+    zeroToSixtyNs = 0;
+    headonDragCoeff = 0.0;
+    horsepower = 0;
+    seatCount = 0;
+    backseatDoors = None;
+}
+
+Car::Car(char const* const manufacturerName, char const* const modelName, PerformanceStats perf, uint8_t numSeats, DoorKind backseatDoorDesign) {
+    manufacturer = new char[strlen(manufacturerName) + 1];
+    strcpy(manufacturer, manufacturerName);
+    
+    model = new char[strlen(modelName) + 1];
+    strcpy(model, modelName);
+    
+    zeroToSixtyNs = perf.zeroToSixtyNs;
+    headonDragCoeff = perf.headonDragCoeff;
+    horsepower = perf.horsepower;
+    seatCount = numSeats;
+    backseatDoors = backseatDoorDesign;
+}
+
+Car::Car(const Car& o) {
+    manufacturer = new char[strlen(o.manufacturer) + 1];
+    strcpy(manufacturer, o.manufacturer);
+    
+    model = new char[strlen(o.model) + 1];
+    strcpy(model, o.model);
+    
+    zeroToSixtyNs = o.zeroToSixtyNs;
+    headonDragCoeff = o.headonDragCoeff;
+    horsepower = o.horsepower;
+    seatCount = o.seatCount;
+    backseatDoors = o.backseatDoors;
+}
+
+Car::~Car() {
+    delete[] manufacturer;
+    delete[] model;
+}
+
+Car& Car::operator=(const Car& o) {
+    if (this != &o) {
+        // Free existing memory
+        delete[] manufacturer;
+        delete[] model;
+
+        // Allocate and copy new memory
+        manufacturer = new char[strlen(o.manufacturer) + 1];
+        strcpy(manufacturer, o.manufacturer);
+
+        model = new char[strlen(o.model) + 1];
+        strcpy(model, o.model);
+
+        zeroToSixtyNs = o.zeroToSixtyNs;
+        headonDragCoeff = o.headonDragCoeff;
+        horsepower = o.horsepower;
+        seatCount = o.seatCount;
+        backseatDoors = o.backseatDoors;
+    }
+    return *this;
+}
+
+char const* Car::getManufacturer() const {
+    return manufacturer;
+}
+
+char const* Car::getModel() const {
+    return model;
+}
+
+///
+
+uint8_t Car::getSeatCount() const {
+    return seatCount;
+}
+
+DoorKind Car::getBackseatDoors() const {
+    return backseatDoors;
+}
+
+void Car::manufacturerChange(char const* const newManufacturer) {
+    delete[] manufacturer;  
+    manufacturer = new char[strlen(newManufacturer) + 1];
+    strcpy(manufacturer, newManufacturer);
+}
+
+void Car::modelNameChange(char const* const newModelName) {
+    delete[] model; 
+    model = new char[strlen(newModelName) + 1];
+    strcpy(model, newModelName);
+}
+
+void Car::reevaluateStats(PerformanceStats newStats) {
+    zeroToSixtyNs = newStats.zeroToSixtyNs;
+    headonDragCoeff = newStats.headonDragCoeff;
+    horsepower = newStats.horsepower;
+}
+
+void Car::recountSeats(uint8_t newSeatCount) {
+    seatCount = newSeatCount;
+}
+
+void Car::reexamineDoors(DoorKind newDoorKind) {
+    backseatDoors = newDoorKind;
+}
